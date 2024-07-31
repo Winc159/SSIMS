393d1b4307acfac3e22a4e7acff01974f2f532fa
Winc159 <1637375142@qq.com>
2024-07-31 15:39:09 +0800

    add teacher pages

--- app/app.js
+++ app/app.js
@@ -46,6 +46,36 @@ app.get("/student/report", function(req, res) {
     res.render("studentpage/report");
 });
 
+// 创建一个 /student/support 路由来渲染支持页面
+app.get("/student/support", function(req, res) {
+    res.render("studentpage/support");
+});
+
+// 创建一个 /teacherinterface 路由来渲染登录页面
+app.get("/teacher/interface", function(req, res) {
+    res.render("teacherpage/interface");
+});
+
+// 创建一个 /teacher/info 路由来渲染个人信息修改页面
+app.get("/teacher/info", function(req, res) {
+    res.render("teacherpage/info");
+});
+
+// 创建一个 /teacher/folder 路由来渲染个人信息修改页面
+app.get("/teacher/folder", function(req, res) {
+    res.render("teacherpage/folder");
+});
+
+// 创建一个 /teacher/timetable 路由来渲染课程表页面
+app.get("/teacher/timetable", function(req, res) {
+    res.render("teacherpage/timetable");
+});
+
+// 创建一个 /teacher/timetable 路由来渲染课程表页面
+app.get("/teacher/edittimetable", function(req, res) {
+    res.render("teacherpage/edit");
+});
+
 // Start server on port 3000
 app.listen(3000,function(){
     console.log(`Server running at http://127.0.0.1:3000/`);
